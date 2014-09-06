05514dc3f24bbd7fc4563739864890fce482d508
Angel Albiach (fusiodarts) <angel@fusiodarts.com>
2014-09-06 22:53:23 +0200

    Se añade variable para configurar puerto noVNC

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,10 +7,12 @@ OPENBOXCFG="$HOME/repos/github/eneboo-guaca/openbox/rc.xml"
 #Rtua a noVNC
 NOVNC="$HOME/repos/github/noVNC"
 
+
+NOPORT=6080
 PORT=5900
 
 Xvfb $DISPLAY -screen 0 1280x1024x24 -pixdepths 1,4,8,12,16,24,32 -shmem &
-DISPLAY=$DISPLAY $NOVNC/utils/launch.sh --vnc 127.0.0.1:$PORT &
+DISPLAY=$DISPLAY $NOVNC/utils/launch.sh --vnc 127.0.0.1:$PORT --listen $NOPORT &
 x11vnc -display $DISPLAY -many -speeds 50,500,15 -nowf -shared -rfbport $PORT &
 
 DISPLAY=$DISPLAY openbox --config-file $OPENBOXCFG &
